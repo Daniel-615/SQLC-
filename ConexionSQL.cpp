c484cf9dc03c492606f1e8ea49b2034d50789a43
Daniel-615 <suyanangel615@gmail.com>
2023-04-20 23:42:07 -0600

    updated

--- ConexionSQL.cpp
+++ ConexionSQL.cpp
@@ -7,7 +7,7 @@ int main()
     connect = mysql_init(0);
     connect = mysql_real_connect(connect, "localhost", "usr_empresa", "Empres@123", "db_empresa", 3306, NULL, 0);
     if (connect) {
-        cout << "Conexion Exitosa"<<endl;
+        cout << "Conexion Exitosa!"<<endl;
     }
     else {
         cout << "Error en la conexion" << endl;
